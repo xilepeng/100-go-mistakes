fc2ce4cec939653140c8b016937a9872e87e9b9c
好饭不怕晚 <lepengxi@163.com>
2025-10-09 06:25:07 +0800

    5 字符串

--- code/05-strings/37-string-iteration/main.go
+++ code/05-strings/37-string-iteration/main.go
@@ -4,17 +4,17 @@ import "fmt"
 
 func main() {
 	s := "hêllo"
-	for i := range s {
+	for i := range s { // 迭代 rune 的每个起始索引，不会迭代每个 rune
 		fmt.Printf("position %d: %c\n", i, s[i])
 	}
 	fmt.Printf("len=%d\n", len(s))
 
-	for i, r := range s {
+	for i, r := range s { // 返回 rune 的起始索引和 rune 本身
 		fmt.Printf("position %d: %c\n", i, r)
 	}
 
-	runes := []rune(s)
-	for i, r := range runes {
+	runes := []rune(s)        //·将字符串转换为 rune 切片
+	for i, r := range runes { // 返回 rune 索引和 rune 本身
 		fmt.Printf("position %d: %c\n", i, r)
 	}
 
