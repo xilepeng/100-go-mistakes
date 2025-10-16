69b3a9d14b7419af49fced0b457e83075f9d39f9
好饭不怕晚 <lepengxi@163.com>
2025-10-16 10:50:34 +0800

    4 控制结构

--- code/04-control-structures/32-range-loop-pointers/customer-store/main.go
+++ code/04-control-structures/32-range-loop-pointers/customer-store/main.go
@@ -25,21 +25,21 @@ func main() {
 
 func (s *Store) storeCustomers(customers []Customer) {
 	for _, customer := range customers {
-		fmt.Printf("%p\n", &customer)
-		s.m[customer.ID] = &customer
+		fmt.Printf("%p\n", &customer) //
+		s.m[customer.ID] = &customer  //只创建一个固定地址的变量
 	}
 }
 
 func (s *Store) storeCustomers2(customers []Customer) {
 	for _, customer := range customers {
-		current := customer
+		current := customer // 创建一个局部变量
 		s.m[current.ID] = &current
 	}
 }
 
 func (s *Store) storeCustomers3(customers []Customer) {
 	for i := range customers {
-		customer := &customers[i]
+		customer := &customers[i] //赋值第i个元素的指针
 		s.m[customer.ID] = customer
 	}
 }
@@ -49,3 +49,13 @@ func print(m map[string]*Customer) {
 		fmt.Printf("key=%s, value=%#v\n", k, v)
 	}
 }
+
+/*
+➜  customer-store git:(main) ✗ go run main.go
+0x1400012a000
+0x1400012a018
+0x1400012a030
+key=1, value=&main.Customer{ID:"1", Balance:10}
+key=2, value=&main.Customer{ID:"2", Balance:-10}
+key=3, value=&main.Customer{ID:"3", Balance:0}
+*/
