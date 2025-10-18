c3a6567827c67c24785a78f6ce3d370761c2128b
好饭不怕晚 <lepengxi@163.com>
2025-10-18 20:10:06 +0800

    12 优化

--- code/12-optimizations/95-stack-heap/main.go
+++ code/12-optimizations/95-stack-heap/main.go
@@ -1,19 +1,24 @@
 package main
 
+// 栈
 func listing1() {
+	// 创建一个栈帧，变量a,b被分配到这个栈帧所在的栈中。
+	// 存储的所有变量都是有效的地址，这意味着它们可以被引用和访问。
 	a := 3
 	b := 2
 
 	c := sumValue(a, b)
-	println(c)
+	println(c) //强制在堆上分配
 }
 
+// 
 //go:noinline
 func sumValue(x, y int) int {
 	z := x + y
 	return z
 }
 
+// 堆
 func listing2() {
 	a := 3
 	b := 2
