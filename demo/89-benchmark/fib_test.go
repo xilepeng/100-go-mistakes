0d8c4311e6d8a0247c1edd1f2357fba41f49189a
好饭不怕晚 <lepengxi@163.com>
2025-10-29 20:42:27 +0800

    11 基准测试benchmark

--- demo/89-benchmark/fib_test.go
+++ demo/89-benchmark/fib_test.go
@@ -2,17 +2,22 @@ package main
 
 import "testing"
 
+var res int
+
 // func BenchmarkFib10(b *testing.B) {
+// 	var r int
 // 	// run the Fib function b.N times
 // 	for n := 0; n < b.N; n++ {
-// 		Fib(10)
+// 		r = Fib(10)
 // 	}
+// 	res = r
 // }
 
-
 func BenchmarkFibFast10(b *testing.B) {
+	var r int
 	// run the Fib function b.N times
 	for n := 0; n < b.N; n++ {
-		FibFast(10)
+		r = FibFast(10)
 	}
+	res = r
 }
